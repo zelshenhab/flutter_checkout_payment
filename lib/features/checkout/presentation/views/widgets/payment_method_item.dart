5785505b185dd45f04612efeb340ee1e890a77f4
Zeyad Elshenhab <z.elshenhab2012@gmail.com>
2024-09-15 23:32:55 +0300

    in active and active payment method

---
+++ lib/features/checkout/presentation/views/widgets/payment_method_item.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+
+class PaymentMethodItem extends StatelessWidget {
+  const PaymentMethodItem({super.key, this.isActive = false});
+  final bool isActive;
+  @override
+  Widget build(BuildContext context) {
+    return AnimatedContainer(
+      duration: const Duration(microseconds: 600),
+      width: 103,
+      height: 62,
+      decoration: ShapeDecoration(
+        shape: RoundedRectangleBorder(
+          side: BorderSide(
+            width: 1.50,
+            color: isActive ? const Color(0xFF34A853) : Colors.grey,
+          ),
+          borderRadius: BorderRadius.circular(15),
+        ),
+        shadows: [
+          BoxShadow(
+              color: isActive ? const Color(0xFF34A853) : Colors.white,
+              blurRadius: 4,
+              offset: const Offset(0, 0),
+              spreadRadius: 0),
+        ],
+      ),
+      child: Container(
+        decoration: BoxDecoration(
+          color: Colors.white,
+          borderRadius: BorderRadius.circular(15),
+        ),
+        child: Center(
+            child: SvgPicture.asset(
+          "assets/images/card.svg",
+          height: 24,
+        )),
+      ),
+    );
+  }
+}
