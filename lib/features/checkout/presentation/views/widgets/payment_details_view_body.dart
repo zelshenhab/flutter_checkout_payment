52d237edf4bf39ff7dbccbfabeaf43899b636a5a
Zeyad Elshenhab <z.elshenhab2012@gmail.com>
2024-09-17 23:12:59 +0300

    create credit card part one

--- lib/features/checkout/presentation/views/widgets/payment_details_view_body.dart
+++ lib/features/checkout/presentation/views/widgets/payment_details_view_body.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_checkout_payment/features/checkout/presentation/views/widgets/payment_methods_list_view.dart';
+import 'package:flutter_credit_card/flutter_credit_card.dart';
 
 class PaymentDetailsViewBody extends StatelessWidget {
   const PaymentDetailsViewBody({super.key});
@@ -9,6 +10,37 @@ class PaymentDetailsViewBody extends StatelessWidget {
     return const Column(
       children: [
         PaymentMethodsListView(),
+        CustomCreditCard(),
+      ],
+    );
+  }
+}
+
+class CustomCreditCard extends StatefulWidget {
+  const CustomCreditCard({
+    super.key,
+  });
+
+  @override
+  State<CustomCreditCard> createState() => _CustomCreditCardState();
+}
+
+class _CustomCreditCardState extends State<CustomCreditCard> {
+  String cardNumber = '', expiryDate = '', cardHolderName = '', cvvCode = '';
+
+  bool showBackView = false;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        CreditCardWidget(
+            cardNumber: cardNumber,
+            expiryDate: expiryDate,
+            cardHolderName: cardHolderName,
+            cvvCode: cvvCode,
+            showBackView: showBackView,
+            onCreditCardWidgetChange: (value) {})
       ],
     );
   }
