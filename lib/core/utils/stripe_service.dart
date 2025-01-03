a2a3b953f7eb98651511ca097234a70b9f639e2e
Zeyad Elshenhab <z.elshenhab2012@gmail.com>
2025-01-04 00:17:49 +0300

    create api service

--- lib/core/utils/stripe_service.dart
+++ lib/core/utils/stripe_service.dart
@@ -1,3 +1,8 @@
+import 'package:flutter_checkout_payment/features/checkout/data/models/payment_intent_input_model.dart';
+import 'package:flutter_checkout_payment/features/checkout/data/models/payment_intent_model/payment_intent_model.dart';
+
 class StripeService {
-  
+  // PaymentIntentModel createPaymentIntent(PaymentIntentInputModel PaymentIntentInputModel){
+
+  // }
 }
