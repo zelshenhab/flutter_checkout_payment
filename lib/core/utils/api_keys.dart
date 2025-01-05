fff5ef214e5d3d1a3e406a4bc73dc6173f00a732
Zeyad Elshenhab <z.elshenhab2012@gmail.com>
2025-01-05 18:43:22 +0300

    implement payment intent

---
+++ lib/core/utils/api_keys.dart
@@ -0,0 +1,4 @@
+class ApiKeys {
+  static String secretKey =
+      'sk_test_51QctVLQKAUSzx7piPXQ5ADGznqTrdnfYRXKwZ2DHo4na3rLrHGjVO5pMa6pfFUFaa3Gj6M8wWpLhqLMhTYL0cKpJ00XMvWTq4l';
+}
--- lib/core/utils/stripe_service.dart
+++ lib/core/utils/stripe_service.dart
@@ -1,8 +1,19 @@
+import 'package:flutter_checkout_payment/core/utils/api_keys.dart';
+import 'package:flutter_checkout_payment/core/utils/api_service.dart';
 import 'package:flutter_checkout_payment/features/checkout/data/models/payment_intent_input_model.dart';
 import 'package:flutter_checkout_payment/features/checkout/data/models/payment_intent_model/payment_intent_model.dart';
 
 class StripeService {
-  // PaymentIntentModel createPaymentIntent(PaymentIntentInputModel PaymentIntentInputModel){
+  final ApiService apiService = ApiService();
+  Future<PaymentIntentModel> createPaymentIntent(
+      PaymentIntentInputModel PaymentIntentInputModel) async {
+    var response = await apiService.post(
+        body: PaymentIntentInputModel.toJson(),
+        url: 'https://api.stripe.com/v1/payment_intents',
+        token: ApiKeys.secretKey);
 
-  // }
+    var paymentIntentModel = PaymentIntentModel.fromJson(response.data);
+
+    return paymentIntentModel;
+  }
 }
